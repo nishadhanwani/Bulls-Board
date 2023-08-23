abdbd48b17398415a77a769791ff81aa26909b95
nishadhanwani <123586341+nishadhanwani@users.noreply.github.com>
2023-08-24 01:37:00 +0530

    Add files via upload

---
+++ stockcode.py
@@ -0,0 +1,18 @@
+import random
+class stocks:
+    stock_dic={('TCS','HDFC Bank','Hindustan Uniliever','M&M'):'big',('Infinity inc.','Wipro','ITC','SBI','NTPC','Tata steel'):'medium',('JPpower','Suzlon energy','Yes Bank','IRFC','Reliance Power'):'penny'}
+    def __init__(self,name,iprice):
+        self.name=name
+        self.iprice=iprice
+    def change(self):
+        for i in stocks.stock_dic:
+            if self.name in i:
+                if(stocks.stock_dic[i]=='big'):
+                    self.iprice+=random.uniform(self.iprice*-0.0003,self.iprice*0.0003)
+                elif(stocks.stock_dic[i]=='medium'):
+                    self.iprice+=random.uniform(self.iprice*-0.0005,self.iprice*0.0005)
+                else:
+                    self.iprice+=random.uniform(self.iprice*-0.0007,self.iprice*0.0007)
+
+
+list_of_stocks=[stocks('Infinity inc.',570.0),stocks('TCS',3501.0),stocks('HDFC Bank',1655.0),stocks('Wipro',406.0),stocks('ITC',383.0),stocks('Hindustan Uniliever',2515.0),stocks('SBI',531.0),stocks('M&M',1341.0),stocks('NTPC',168.0),stocks('Tata steel',112.0),stocks('JPpower',6.8),stocks('Suzlon energy',8.9),stocks('Yes Bank',16.1),stocks('IRFC',28.75),stocks('Reliance Power',11.05)]
---
+++ user.py
@@ -0,0 +1,109 @@
+
+import csv
+import pandas as pd
+class user:
+    stocks=[]
+    def __init__(self,username,passw):
+        self.username=username
+        self.passw=passw
+        self.uf=open('new.csv','r')
+        self.cr=csv.DictReader(self.uf)
+      
+
+        check=0
+        for i in self.cr:
+            
+            if(i['Username']==self.username):
+                check+=1
+                if(i['Password']==self.passw):
+                    check+=1
+                    print("LOGINNED SUCCESFUULY")
+            
+        if(check==0):
+            print('Username not found')
+            self.temp=0
+            print('Do you want to 1.create new user or 2.try again? or 3.Exit')
+            self.temp=int(input('Choice='))
+            if(self.temp==1):
+                user.createuser(self)
+                
+            elif(self.temp==2):
+                temp_user=input('username=')
+                temp_pass=input('password=')
+                user.__init__(self,temp_user,temp_pass)
+            else:
+                print("THEEK HAI BHAI")
+                exit()
+        if(check==1):
+            print('Password invalid Try again?')
+            self.temp=(input('Choice(yes/no)='))
+            if(self.temp=='yes'):
+                temp_user=input('username=')
+                temp_pass=input('password=')
+                user.__init__(self,temp_user,temp_pass)
+                
+            else:
+                print("THEEK HAI BHAI")
+                exit()
+        self.uf.close()
+    def createuser(self):
+        self.newuser=input('Username=')
+        self.uf=open('new.csv','r')
+        self.cr=csv.DictReader(self.uf)
+        self.check=0
+        for i in self.cr:
+            
+            if(i['Username']==self.newuser):
+                self.check+=1
+        if(self.check==1):
+            print('This username already exists')
+            user.createuser(self)
+        else:
+            self.newpass=input('Password=')
+            self.newbal=float(input("Initial balance="))
+            uf=open('new.csv','a',newline='')
+            cw=csv.writer(uf)
+            cw.writerow([self.newuser,self.newpass,self.newbal])
+            uf.close()
+        self.uf.close()
+    def getbalance(self):
+        self.filvar=open('new.csv','r')
+        self.csvar=csv.DictReader(self.filvar)
+        for j in self.csvar:
+            if(j['Username']==self.username):
+                return float(j['Balance'])
+                self.filvar.close()
+        
+    def changepass(self):
+        csvread=pd.read_csv('new.csv')
+        rowindex=csvread.loc[csvread['Username']==self.username].index[0]
+        csvread.loc[rowindex,'Password']=input('New Password=')
+        csvread.to_csv('new.csv',index=False)
+    def changebal(self,tran_money):
+        self.tran_money=tran_money
+        csvread=pd.read_csv('new.csv')
+        rowindex=csvread.loc[csvread['Username']==self.username].index[0]
+        csvread.loc[rowindex,'Balance']=user.getbalance(self)+self.tran_money
+        csvread.to_csv('new.csv',index=False)
+class bot:
+    def __init__(self,botname):
+        self.botname=botname
+    def getbalance(self):
+        self.filvar=open('new.csv','r')
+        self.csvar=csv.DictReader(self.filvar)
+        for j in self.csvar:
+            if(j['Username']==self.botname):
+                return float(j['Balance'])
+                self.filvar.close()
+    def changebal(self,tran_money):
+        self.tran_money=tran_money
+        csvread=pd.read_csv('new.csv')
+        rowindex=csvread.loc[csvread['Username']==self.botname].index[0]
+        csvread.loc[rowindex,'Balance']=bot.getbalance(self)+self.tran_money
+        csvread.to_csv('new.csv',index=False)
+        
+#user_name=input('username=')
+#pass_word=input('password=')
+#u1=user(user_name,pass_word)
+#u1.changepass()
+#u1.createuser()
